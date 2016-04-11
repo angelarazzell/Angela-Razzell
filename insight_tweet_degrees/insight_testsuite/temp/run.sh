eed0b314009499200bd5f9443ff2c10f4a5a5b5f
angelarazzell <angela.razzell@gmx.com>
2016-04-11 07:10:04 -0400

    Delete run.sh

--- insight_tweet_degrees/insight_testsuite/temp/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-python ./src/tweets_extract_fields.py ./tweet_input/tweets.txt ./tweet_input/tweets_extract.txt
-
-python ./src/average_degree.py ./tweet_input/tweets_extract.txt ./tweet_output/output.txt
-
-
-
-
